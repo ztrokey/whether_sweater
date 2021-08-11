4be6af34f1105baa19fbe4c9fe6c0d4006118260
Zach T <ztrokey@gmail.com>
2021-08-10 19:10:37 -0600

    add sad paths

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,7 +2,14 @@ class Api::V1::ForecastController < ApplicationController
   def index
     location = params[:location]
 
-    forecast = ForecastFacade.get_forecast(location)
-    render json: ForecastSerializer.new(forecast)
+    if params[:location].blank?
+      render json: {
+        error: 'location cannot be blank',
+        status: 406
+      }, status: 406
+    else
+      forecast = ForecastFacade.get_forecast(location)
+      render json: ForecastSerializer.new(forecast)
+    end
   end
 end
